961bc8628596cbb35539824b3ec97320841b6ced
Ravi Bhatt <ravi.bhatt.754918@gmail.com>
2018-10-07 20:50:06 +0530

    report enlarged

--- js/script.js
+++ js/script.js
@@ -7,12 +7,20 @@ function get_report (location) {
     if (this.readyState == 4 && this.status == 200) {
       JSONdata=this.responseText;
       console.log(JSONdata);
-      var json_obj = JSON.parse(JSONdata)
-      var display_element = document.getElementById('text_area_container');
-      display_element.style.display = "flex";
-      var display_data = "temperature = " + json_obj.main.temp + " °C";
-      console.log(display_data);
-      document.getElementById('output_text_area').innerHTML = display_data;
+      var json_obj = JSON.parse(JSONdata);
+      var tile1 = document.getElementById('display_tile_1');
+      var tile2 = document.getElementById('display_tile_2');
+      var tile3 = document.getElementById('display_tile_3');
+      var tile4 = document.getElementById('display_tile_4');
+      tile1.style.display="inline";
+      tile2.style.display="inline";
+      tile3.style.display="inline";
+      tile4.style.display="inline";
+      tile1.textContent  = "temperature : " + json_obj.main.temp + " °C";
+      tile2.textContent = "min : " + json_obj.main.temp_min+ " °C"+"   max : " +json_obj.main.temp_max+ " °C"
+      tile3.textContent = json_obj.weather[0].main+" : "+json_obj.weather[0].description;
+	  tile4.textContent = "lon : " +json_obj.coord.lon + " , lat : " +json_obj.coord.lat;
+
     }
   };
 
